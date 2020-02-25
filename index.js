d7d65200c0625ec703c24788929b4125943ba654
didwjddn <didwjddn@konkuk.ac.kr>
2020-02-25 15:00:06 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://C:1234@cluster0-attdu.mongodb.net/test?retryWrites=true&w=majority',
+{useNewUrlParser:true}).then(()=> console.log("DB connected"))
+.catch(err=>console.error(err));
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+});
+
+app.listen(5000);
