2c1b8a3e0870244495488d7fe40709286d87ef73
didwjddn <didwjddn@konkuk.ac.kr>
2020-02-25 16:32:08 +0900

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express =require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://C:1234@cluster0-attdu.mongodb.net/test?retryWrites=true&w=majority',
+const bodyParser =require('body-parser');
+const cookieParser =require('cookie-parser');
+
+const config = require('./config/key');
+
+const {User} = require('./models/user');
+
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true}).then(()=> console.log("DB connected"))
 .catch(err=>console.error(err));
 
 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.json({"hello ~": "hi~~"})
 });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
+app.post("/api/users/register",(req, res)=>{
+
+    const user = new User(req.body);
+
+        user.save((err, userDate)=> {
+            if(err) return res.json({success: false, err})
+        })
+
+    return res.status(200).json({
+        success:true
+    })
+})
+
+
+
 
 app.listen(5000);
